12f30dae5a30a6ac85bab76866ee1ee35421d950
Ishara-MacDonald <ishara.macdonald@student.hu.nl>
2022-01-03 17:33:16 +0100

    Small Changes

--- src/views/Assignments.vue
+++ src/views/Assignments.vue
@@ -1,33 +1,36 @@
 <template>
-    <div id="assignment-view">
-        <h1>Our assignments</h1>
-        <h2>Nothing to book right now. Check back soon.</h2>
-    </div>
+  <div id="assignment-view">
+    <h1>Our assignments</h1>
+    <h2>Nothing to book right now. Check back soon!</h2>
+  </div>
 </template>
 
 <style scoped>
-#assignment-view{
-    margin:0 auto;
-    padding: 100px 0px 0px 0px;
-    width:90%;
-    height: auto;
-    min-height:650px;
-    background-color: white;
-    color: black;
-    text-align: center;
+#assignment-view {
+  margin: 0 auto;
+  padding-bottom: 60px;
+  border: 60px solid black;
+  border-bottom: 0;
+  border-top: 0;
+  width: 90%;
+  height: auto;
+  min-height: 650px;
+  background-color: white;
+  color: black;
+  text-align: center;
 }
 
-h1{
-    font-weight: bolder;
-    font-size: 60px;
-    letter-spacing: 1px;
-    padding:40px 0px;
+h1 {
+  font-weight: bolder;
+  font-size: 50px;
+  letter-spacing: 1px;
+  padding: 30px 0px;
 }
 
-h2{
-    font-weight: bold;
-    font-size: 30px;
-    padding:40px 0px;
+h2 {
+  font-size: 30px;
+  padding: 20px 0px;
+  letter-spacing: 1.5px;
+  font-weight: normal;
 }
-
 </style>
