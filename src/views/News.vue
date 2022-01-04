efed7876a43c57e8a358b76b7327f04c2eb8b69c
Ishara-MacDonald <ishara.macdonald@student.hu.nl>
2022-01-04 15:31:02 +0100

    Updated Style News

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="news-overview">
+    <h1>Nieuws</h1>
     <div class="most-recent-news">
-      <h1>BeeConnected Services is live!</h1>
-      <p>
-        It's official!<br/>
-        BeeConnected Services is now an established company, providing expertise
-        and knowhow to create, manage all network and security challenges for
-        all types of companies throughout The Netherlands and abroad.
-      </p>
+      <div class="image-text"></div>
+      <div class="news-text">
+        <h2 class="news-text-title">BeeConnected Services is live!</h2>
+        <p>
+          It's official!<br/>
+          BeeConnected Services is now an established company, providing expertise
+          and knowhow to create, manage all network and security challenges for
+          all types of companies throughout The Netherlands and abroad.
+        </p>
+      </div>
     </div>
+    <h1 class="portfolio-title">Onze Nieuwe Portfolio</h1>
     <img src="../assets/beeconnected-core.webp" alt="core" />
     <div>
-      <h1>Meer Weten?</h1>
+      <h2>Meer Weten?</h2>
+      <h3>Duik dieper in de wereld van BeeConnected IT Services</h3>
     </div>
   </div>
 </template>
@@ -25,32 +31,50 @@
   color: black;
   text-align: center;
 }
-#news-overview h1 {
-  font-weight: bolder;
-  font-size: 50px;
+h1 {
+  font-weight: normal;
+  font-size: 70px;
   letter-spacing: 1px;
+  padding: 40px 0px;
+  padding: 70px 0px;
+}
+h2{
+  padding-top: 100px;
+  font-size: 50px;
+  font-weight: normal;
+}
+h3{
+  font-weight: normal;
+  font-size: 25px;
   padding: 30px 0px;
 }
 
 .most-recent-news {
+  display:grid;
+  grid-template-columns: 35% 65%;
+}
+
+.news-text{
   background-color: rgb(51, 137, 207);
-  height: auto;
+  height: 500px;
   width: 100%;
   margin: 0 auto;
-  padding: 30px 20px;
+  padding: 30px 0px;
   color: white;
 }
 
-p {
+.image-text{
+  background-image: url("../assets/start-button-rocket.jpg");
+  background-size: 100% 100%;
+}
+.news-text p {
   line-height: 35px;
   font-size: 20px;
   letter-spacing: 0.5px;
-  padding: 0 450px;
-  padding-bottom: 30px;
+  padding: 40px 200px;
 }
 img {
-  margin-top: 100px;
-  width: 700px;
+  width: 800px;
   height: 100%;
   border: 2px solid rgb(177, 177, 177);
 }
