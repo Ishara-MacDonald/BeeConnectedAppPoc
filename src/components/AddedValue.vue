e86ccdf7152cb25e9272dccdf003c5fbf7b7d839
Ishara-MacDonald <ishara.macdonald@student.hu.nl>
2022-01-04 15:40:43 +0100

    Updated Home - IT Services

--- src/components/AddedValue.vue
+++ src/components/AddedValue.vue
@@ -1,48 +1,61 @@
 <template>
-    <div id="added-value">
-        <div id="value-container">
-            <div id="value-picture"></div>
-            <div id="value-info">
-                <h1>Added Value</h1>
-                <h2>Network Engineer</h2>
-            </div>
-        </div>
+  <div id="added-value">
+    <div id="value-container">
+      <div id="value-picture"></div>
+      <div id="value-info">
+        <h1>IT Services:</h1>
+        <ul>
+          <li>
+            <h2>Routing & Switching</h2>
+          </li>
+          <li>
+            <h2>Security</h2>
+          </li>
+          <li>
+            <h2>Network Operations</h2>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 </script>
 
 <style scoped>
-#added-value{
-    height:700px;
-    width:100%;
+#added-value {
+  height: 600px;
+  width: 100%;
 }
-#value-container{
-    display: grid;
-    width:80%;
-    height:100%;
-    grid-template-columns: 60% 40%;
-    margin: 0 auto;
+#value-container {
+  display: grid;
+  width: 80%;
+  height: 100%;
+  grid-template-columns: 55% 45%;
+  margin: 0 auto;
 }
-#value-picture{
-    background-image: url("../assets/added-value-hand-shaking.png");
-    background-size: 100% 100%;
+#value-picture {
+  background-image: url("../assets/services-honeycomb-man-in-suit.png");
+  background-size: 100% 100%;
 }
-#value-info{
-    background-color: rgb(31, 31, 31);
-    padding:58px 120px;
+#value-info {
+  background-color: rgb(31, 31, 31);
+  padding: 58px 100px;
+}
+#value-info li {
+  padding: 5px;
 }
 
-#value-info h1, h2, h3, h4, h5, h6{
-    font-weight: normal;
-    letter-spacing: 3px;
+#value-info h1, h2, h3, h4, h5, h6 {
+  font-weight: normal;
+  letter-spacing: 3px;
 }
-#value-info h1{
-    font-size: 35pt;
+#value-info h1 {
+  font-size: 35pt;
 }
-#value-info h2{
-    font-size: 22pt;
-    margin:30px 0px;
+#value-info h2 {
+  font-size: 25pt;
+  margin: 30px 0px;
 }
 </style>
