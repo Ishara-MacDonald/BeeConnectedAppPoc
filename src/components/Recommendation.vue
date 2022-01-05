275b82602201e32856e0af6252bfc3ad4c98a16d
Ishara-MacDonald <ishara.macdonald@student.hu.nl>
2022-01-05 14:14:13 +0100

    Created Recommendatrion

---
+++ src/components/Recommendation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-show="visibleRec === index" class="recommendation"></div>
+</template>
+
+<script>
+export default {
+  name: 'Recommendation',
+  props: ['visibleRec', 'index'],
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+</style>
